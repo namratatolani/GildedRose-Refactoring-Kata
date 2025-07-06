eaea727f0abfa5f435a72ebb336766b5caa9d513
Namrata Tolani <nams.tolani@gmail.com>
2025-07-06 15:03:41 +0000

    init refactor

---
+++ python/refactored_rose.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            item.sell_in -= 1
+            item.quality -= 1
+
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
